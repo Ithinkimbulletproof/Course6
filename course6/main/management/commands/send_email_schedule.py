6560d9475f624c8c86723ab9714cbcbed13806fa
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-06-23 14:09:32 +0700

    some fix

--- course6/main/management/commands/send_email_schedule.py
+++ course6/main/management/commands/send_email_schedule.py
@@ -2,23 +2,21 @@ from django.core.management.base import BaseCommand
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import schedule
-import time
 
 
 class Command(BaseCommand):
-    help = 'Sending email on schedule'
+    help = 'Sending email command'
 
-    def send_email(self):
+    def handle(self, *args, **options):
         from_email = "your_email@gmail.com"
         to_email = "recipient_email@gmail.com"
 
         msg = MIMEMultipart()
         msg['From'] = from_email
         msg['To'] = to_email
-        msg['Subject'] = "Тема письма"
+        msg['Subject'] = "Subject of the email"
 
-        body = "Текст сообщения"
+        body = "Body of the email"
         msg.attach(MIMEText(body, 'plain'))
 
         server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -26,10 +24,3 @@ class Command(BaseCommand):
         server.login(from_email, "your_password")
         server.send_message(msg)
         server.quit()
-
-    def handle(self, *args, **options):
-        schedule.every().day.at("10:00").do(self.send_email)
-
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
