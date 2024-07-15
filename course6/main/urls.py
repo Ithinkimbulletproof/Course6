7812ea85b79b66b6116db0968c713b28b5c45d11
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-07-15 12:55:44 +0700

    Add bootstrap

--- course6/main/urls.py
+++ course6/main/urls.py
@@ -2,21 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Маршруты для Client
     path('clients/', views.client_list, name='client_list'),
     path('clients/<int:pk>/', views.client_detail, name='client_detail'),
     path('clients/create/', views.client_create, name='client_create'),
     path('clients/<int:pk>/edit/', views.client_update, name='client_update'),
     path('clients/<int:pk>/delete/', views.client_delete, name='client_delete'),
 
-    # Маршруты для Message
     path('messages/', views.message_list, name='message_list'),
     path('messages/<int:pk>/', views.message_detail, name='message_detail'),
     path('messages/create/', views.message_create, name='message_create'),
     path('messages/<int:pk>/edit/', views.message_update, name='message_update'),
     path('messages/<int:pk>/delete/', views.message_delete, name='message_delete'),
 
-    # Маршруты для Mailing
     path('mailings/', views.mailing_list, name='mailing_list'),
     path('mailings/<int:pk>/', views.mailing_detail, name='mailing_detail'),
     path('mailings/create/', views.mailing_create, name='mailing_create'),
